29a3b44aaebc33213d09855f4601f5f05f65032a
Wilton Souza <willsouzafilho@gmail.com>
2022-03-31 11:37:26 -0300

    Adding password encrypt with bcryptjs

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,3 +1,4 @@
+import { hash } from "bcryptjs";
 import { inject, injectable } from "tsyringe";
 
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
@@ -16,9 +17,16 @@ class CreateUserUseCase {
     password,
     driver_license,
   }: ICreateUserDTO): Promise<void> {
+    const emailAlreadyUsed = await this.usersRepository.findByEmail(email);
+
+    if (emailAlreadyUsed) {
+      throw new Error("User already exists!");
+    }
+    const passwordHash = await hash(password, 8);
+
     await this.usersRepository.create({
       name,
-      password,
+      password: passwordHash,
       email,
       driver_license,
     });
