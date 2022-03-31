e1cf87ed366ea0a0b703a5d0656b25c8fe80ae2e
Wilton Souza <willsouzafilho@gmail.com>
2022-03-31 06:58:05 -0300

    Applying dependency inversion to all useCases

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 
-import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
+// eslint-disable-next-line max-len
+import { CreateSpecificationController } from "../modules/cars/useCases/createSpecification/CreateSpecificationController";
 
 const specificationsRoutes = Router();
 
-specificationsRoutes.post("/", (request, response) => {
-  return createSpecificationController.handle(request, response);
-});
+const createSpecificationController = new CreateSpecificationController();
+
+specificationsRoutes.post("/", createSpecificationController.handle);
 
 export { specificationsRoutes };
--- src/server.ts
+++ src/server.ts
@@ -3,20 +3,25 @@ import swaggerUi from "swagger-ui-express";
 import YAML from "yamljs";
 
 import { AppDataSource } from "./database";
+import "./shared/container";
 import { router } from "./routes";
 
 AppDataSource.initialize()
   .then(() => {
-    const swaggerFile = YAML.load(`${__dirname}/swagger.yaml`);
-
-    const app = express();
-
-    app.use(express.json());
-
-    app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
-
-    app.use(router);
-
-    app.listen(3333, () => console.log("Server is running!"));
+    console.log("Data Source has been initialized!");
   })
-  .catch((error) => console.log(error));
+  .catch((err) => {
+    console.error("Error during Data Source initialization", err);
+  });
+
+const swaggerFile = YAML.load(`${__dirname}/swagger.yaml`);
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+app.use(router);
+
+app.listen(3333, () => console.log("Server is running!"));
