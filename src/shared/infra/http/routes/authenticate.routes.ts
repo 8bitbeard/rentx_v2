2bdfc6d10fd22ba42a91556d341f4250894f67a7
Wilton Souza <willsouzafilho@gmail.com>
2022-04-01 17:39:09 -0300

    Fixing import errors

--- src/shared/infra/http/routes/authenticate.routes.ts
+++ src/shared/infra/http/routes/authenticate.routes.ts
@@ -2,8 +2,6 @@ import { Router } from "express";
 
 import { AuthenticateUserController } from "@modules/accounts/useCases/createAuthentication/AuthenticateUserController";
 
-// eslint-disable-next-line max-len
-
 const authenticateRoutes = Router();
 
 const authenticateUserController = new AuthenticateUserController();
