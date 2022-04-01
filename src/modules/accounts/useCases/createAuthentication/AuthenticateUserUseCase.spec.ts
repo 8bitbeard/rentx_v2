831bb6b8a755c8610207443f21507d6cd351aedc
Wilton Souza <willsouzafilho@gmail.com>
2022-04-01 14:17:12 -0300

    Adding unit tests for createUserUseCase and authenticateUserUseCase

---
+++ src/modules/accounts/useCases/createAuthentication/AuthenticateUserUseCase.spec.ts
@@ -0,0 +1,66 @@
+import { AppError } from "../../../../shared/errors/AppError";
+import { UsersRepositoryInMemory } from "../../repositories/in-memory/UsersRepositoryInMemory";
+import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
+import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
+
+describe("Authenticate User", () => {
+  let authenticateUserUseCase: AuthenticateUserUseCase;
+  let createUserUseCase: CreateUserUseCase;
+  let usersRepositoryInMemory: UsersRepositoryInMemory;
+  beforeEach(() => {
+    usersRepositoryInMemory = new UsersRepositoryInMemory();
+    createUserUseCase = new CreateUserUseCase(usersRepositoryInMemory);
+    authenticateUserUseCase = new AuthenticateUserUseCase(
+      usersRepositoryInMemory
+    );
+  });
+
+  it("should be able to authenticate a valid user", async () => {
+    const userData = {
+      name: "Unit Test User",
+      email: "unit_test@example.com",
+      password: "123456",
+      driver_license: "ABC1234",
+    };
+
+    await createUserUseCase.execute(userData);
+
+    const result = await authenticateUserUseCase.execute({
+      email: userData.email,
+      password: userData.password,
+    });
+
+    expect(result).toHaveProperty("token");
+    expect(result.user.name).toEqual(userData.name);
+    expect(result.user.email).toEqual(userData.email);
+  });
+
+  it("should not be able to authenticate an inexistent user", async () => {
+    const userData = {
+      email: "inexistent@example.com",
+      password: "123456",
+    };
+
+    await expect(authenticateUserUseCase.execute(userData)).rejects.toEqual(
+      new AppError("Email or password incorrect!")
+    );
+  });
+
+  it("should not be able to authenticate with an invalid password", async () => {
+    const userData = {
+      name: "Unit Test User",
+      email: "unit_test@example.com",
+      password: "123456",
+      driver_license: "ABC1234",
+    };
+
+    await createUserUseCase.execute(userData);
+
+    await expect(
+      authenticateUserUseCase.execute({
+        email: userData.email,
+        password: "invalid",
+      })
+    ).rejects.toEqual(new AppError("Email or password incorrect!"));
+  });
+});
---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,39 @@
+import { AppError } from "../../../../shared/errors/AppError";
+import { CategoriesRepositoryInMemory } from "../../repositories/in-memory/CategoriesRepositoryInMemory";
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
+
+describe("Create Category", () => {
+  let createCategoryUseCase: CreateCategoryUseCase;
+  let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
+  beforeEach(() => {
+    categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
+    createCategoryUseCase = new CreateCategoryUseCase(
+      categoriesRepositoryInMemory
+    );
+  });
+
+  it("should be able to create a new category", async () => {
+    const categoryData = {
+      name: "Unit Test Category",
+      description: "Unit Test Description",
+    };
+    await createCategoryUseCase.execute(categoryData);
+
+    const createdCategory = await categoriesRepositoryInMemory.findByName(
+      categoryData.name
+    );
+
+    expect(createdCategory).toHaveProperty("id");
+  });
+
+  it("should not be able to create a new category with an already used name", async () => {
+    const categoryData = {
+      name: "Unit Test Category",
+      description: "Unit Test Description",
+    };
+    await createCategoryUseCase.execute(categoryData);
+    await expect(createCategoryUseCase.execute(categoryData)).rejects.toEqual(
+      new AppError("Category already exists!")
+    );
+  });
+});
